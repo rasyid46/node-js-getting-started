0a93331f868ec1440239c271166655c1bf1a445c
Sulaeman <sulaeman@rasyid.com>
2019-10-25 17:45:10 +0700

    Mongo Crud

--- src/routes/routeMongo.js
+++ src/routes/routeMongo.js
@@ -1,14 +1,22 @@
 const Models = require('../../models/index') 
 const Joi = require('@hapi/joi');
+const Mongoose = require("mongoose");
 
-         
+Mongoose.connect("mongodb://localhost/belajarmongo");
+const PersonModel = Mongoose.model("person", {
+    firstname: String,
+    lastname: String
+});  
 var MongoClient = require('mongodb').MongoClient;
 const todosHandler = async (request, h) => {
         try {
+
+            var person = await PersonModel.find().exec();
             const response = {
                 statusCode : 200,
                 error : "",
                 message : "List mongo", 
+                content : person
             } 
             return h.response(response).code(200)
         } catch (error) {
@@ -20,9 +28,89 @@ const todosHandler = async (request, h) => {
             return h.response(response).code(400)
         }
     }
+    const insertHandler = async (request, h) => {
+            try {
+           
+                var person = new PersonModel(request.payload);
+                var result = await person.save();
+                const response = {
+                    statusCode : 200,
+                    error : "",
+                    message : "List mongo", 
+                    content : result
+                } 
+                return h.response(result).code(200)
+            } catch (error) {
+                const response = {
+                    statusCode : 400,
+                    error : "",
+                    message : error.message, 
+                } 
+                return h.response(response).code(400)
+            }
+    }
+    const updateHandler = async (request,h) => {
+         try{
+            var result = await PersonModel.findByIdAndUpdate(request.params.id, request.payload, { new: true });
+            const response = {
+                statusCode : 200,
+                error : "",
+                message : "List update",
+                content : result
+            } 
+            return h.response(response).code(200)
+         }catch(error){
+            const response = {
+                statusCode : 400,
+                error : "",
+                message : error.message, 
+            } 
+            return h.response(response).code(400)
+         }
+    }    
+    const getMonogDetail = async(request, h) => {
+        try{
+            var person = await PersonModel.findById(request.params.id).exec();
+            const response = {
+                statusCode : 200,
+                error : "",
+                message : "List update",
+                content : person
+            } 
+            return h.response(response).code(200)
+         }catch(error){
+            const response = {
+                statusCode : 400,
+                error : "",
+                message : error.message, 
+            } 
+            return h.response(response).code(400)
+         }
+    }
+    const deletePerson = async(request, h) => {
+        try {
+            // var person = await PersonModel.findById(request.params.id).exec();
+            var result = await PersonModel.findByIdAndDelete(request.params.id);
+            const response = {
+                statusCode : 200,
+                error : "",
+                message : "Delete Person",
+                content : result
+            } 
+            return h.response(response).code(200)
+        } catch (error) {
+            const response = {
+                statusCode : 400,
+                error : "",
+                message : error.message, 
+            } 
+            return h.response(response).code(400)
+        }
+    }
 module.exports = [
         { method: 'GET', path: '/mongo/list', handler: todosHandler },
-        { method: 'POST',path: '/mongo/create',handler: todosHandler },
-        { method: 'PUT', path: '/mongo/update/{id}', handler: todosHandler },
-        { method: 'DELETE', path: '/mongo/delete/{id}', handler: todosHandler },
+        { method: 'GET', path: '/mongo/list/{id}', handler: getMonogDetail },
+        { method: 'POST',path: '/mongo/create',handler: insertHandler },
+        { method: 'PUT', path: '/mongo/update/{id}', handler: updateHandler },
+        { method: 'DELETE', path: '/mongo/delete/{id}', handler: deletePerson },
     ];
