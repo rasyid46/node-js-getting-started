f970b7160cf2559f07e1399ec7fcd608d8c4a30f
Sulaeman <sulaeman@rasyid.com>
2019-10-26 08:23:19 +0700

    mongoHapi

--- mongoModel/mongoConfig.js
+++ mongoModel/mongoConfig.js
@@ -1,5 +1,7 @@
 require('dotenv').config()
-
 const Mongoose = require("mongoose");
-Mongoose.connect("mongodb://localhost/belajarmongo");
+const MONGODB_URI = process.env.MONGODB_URI
+Mongoose.connect(MONGODB_URI);
+// Mongoose.connect("mongodb://heroku_cmqlnzld:admin1234!@ds241097.mlab.com:41097/heroku_cmqlnzld?retryWrites=false");
 module.exports = Mongoose;
+
