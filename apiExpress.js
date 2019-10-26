313852abafe546a5b20f0eb59972aeb6bc15e396
Sulaeman <sulaeman@rasyid.com>
2019-10-26 08:47:33 +0700

    API

---
+++ apiExpress.js
@@ -0,0 +1,8 @@
+require('dotenv').config()
+const express = require('express')
+const path = require('path')
+const app = express()
+const PORT = process.env.PORT || 5000
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
