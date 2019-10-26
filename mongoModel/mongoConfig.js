824b670ee733e2fd9f9d03aa706cb1cd302bba99
Sulaeman <sulaeman@rasyid.com>
2019-10-26 21:53:10 +0700

    A

--- mongoModel/mongoConfig.js
+++ mongoModel/mongoConfig.js
@@ -1,11 +1,8 @@
 require('dotenv').config()
 const Mongoose = require("mongoose");
-const MONGODB_URI = process.env.MONGODB_URI
-const TYPE = process.env.TYPE || "PROD"
-if(TYPE==="LOCAL"){
-    Mongoose.connect(MONGODB_URI);
-}else{
-    Mongoose.connect("mongodb://heroku_cmqlnzld:admin1234!@ds241097.mlab.com:41097/heroku_cmqlnzld?retryWrites=false");
-}
+const MONGODB_URI = process.env.MONGODB_URI 
+
+Mongoose.connect("mongodb://heroku_cmqlnzld:admin1234!@ds241097.mlab.com:41097/heroku_cmqlnzld?retryWrites=false");
+
 module.exports = Mongoose;
 
