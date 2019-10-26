9c0a177f7235d9c9ab6497d9c856d69693b9dd25
Sulaeman <sulaeman@rasyid.com>
2019-10-26 22:15:21 +0700

    Cors

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 require('dotenv').config()
 
 const Joi = require('@hapi/joi');
+const cors = require('hapi-cors')
 const PORT = process.env.PORT  
 
 // express()
@@ -16,7 +17,17 @@ const Hapi = require('@hapi/hapi');
 const init = async () => {
 
     const server = Hapi.server({
-        port: PORT
+        port: PORT,
+        routes: {
+          cors: {
+              origin: ['*'], // an array of origins or 'ignore'
+              headers: ['Authorization'], // an array of strings - 'Access-Control-Allow-Headers' 
+              exposedHeaders: ['Accept'], // an array of exposed headers - 'Access-Control-Expose-Headers',
+              additionalExposedHeaders: ['Accept'], // an array of additional exposed headers
+              maxAge: 60,
+              credentials: true // boolean - 'Access-Control-Allow-Credentials'
+          }
+      }
     });
 
     server.route([{
